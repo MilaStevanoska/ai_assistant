bfc623f09d907bb80eebcc63b840770ef5a8fefb
unknown <andreh.tasevski@gmail.com>
2025-04-27 01:13:22 +0200

    Added UserExport

---
+++ src/CareerCompass.DataContext/Converters/AlternativesConverter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCompass.DataContext.Converters
+{
+    public class AlternativesConverter : ValueConverter<List<string>, string>
+    {
+        private static readonly char[] separator = [','];
+
+        public AlternativesConverter() 
+            : base(
+            list => ConvertToString(list),       
+            value => ConvertToList(value)
+            )         
+        { }
+
+        private static string ConvertToString(List<string> list)
+        {
+            return list == null ? string.Empty : string.Join(",", list);
+        }
+
+        private static List<string> ConvertToList(string value)
+        {
+            return string.IsNullOrWhiteSpace(value) ? new List<string>() : value.Split(separator).ToList();
+        }
+    }
+}
---
+++ src/CareerCompass.DataContext/Entities/UserExport.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCompass.DataContext.Entities
+{
+    public class UserExport
+    {
+        public UserExport()
+        {
+        }
+        public UserExport(int id, String jobTitle, String why, String salaryRange, String educationPath, List<String> alternatives)
+        {
+            Id = id;
+            JobTitle = jobTitle;
+            Why = why;
+            SalaryRange = salaryRange;
+            EducationPath = educationPath;
+            Alternatives = alternatives;
+        }
+
+        [Key]
+        public int Id { get; set; }  
+
+        public string JobTitle { get; set; }
+
+        public string Why { get; set; }
+
+        public string SalaryRange { get; set; }
+
+        public string EducationPath { get; set; }
+
+        public List<string> Alternatives { get; set; }
+    }
+}
